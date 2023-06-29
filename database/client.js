4c8b763821294bc5d05e90a182db82b748604bce
Justin Keowen <jkeowen@gmail.com>
2023-06-28 20:59:57 -0500

    setup pg and ts

---
+++ database/client.js
@@ -0,0 +1,5 @@
+"use strict";
+// const { Client } =  require('pg');
+Object.defineProperty(exports, "__esModule", { value: true });
+// const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/banking_app');
+console.log('test');
